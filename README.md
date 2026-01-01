91d150c5c316f4c2ec8ede1f2674482e40553e3f
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2026-01-01 21:24:39 +0545

    1st Day

---
+++ README.md
@@ -0,0 +1,23 @@
+## **One Months Challenge Next JS**
+
+**Happy New Year 2026**
+
+# JavaScript
+
+- Programming Language
+- Runs on browser
+- High-Level Programming Language
+- Interpreted Programmin Language
+- Dynamically typed programming language
+- used to build interactive web pages
+
+# -- ECMA = European Computer Manufacturers Association
+
+- ECMA Script (ES6-2015 Drastical Changes )
+  _"W3School"_
+
+# Node.Js (single Thread application )
+
+- Run Time of javascript
+- Electro.js (Desktop Application)
+- React Native (Mobile Application)
---
+++ main.js
@@ -0,0 +1,60 @@
+console.warn("This is a warning message");
+console.error("This is an error message");
+console.info("This is an info message");
+console.log("This is a log message");
+
+// var, let, const
+
+const age = 25; //Reading Int
+const name = "Yubraj Dhakal"; // Reading String
+const isMale = true; // Reading Boolean
+
+let address = "Bardaghat-09, Nawalparasi";
+address = "Bhairahawa";
+
+var city = "Bhairahawa";
+var city = "Bardaghat";
+
+console.log(age);
+console.log(name);
+console.log(isMale);
+console.log(address);
+console.log(city);
+
+// Operators
+// Arithmetic Operators
+
+// 4. Ternary Operator : <condition> ? <value_if_true> : <value_if_false>
+
+const percentage = 40;
+
+console.log(percentage >= 40 ? "Pass" : "Fail");
+
+// Conditional Statements
+if (percentage < 40) {
+  console.log("Fail");
+} else if (percentage < 60) {
+  console.log("Second Devision");
+} else if (percentage < 80) {
+  console.log("First Devision");
+} else if (percentage < 100) {
+  console.log("Distriction Devision");
+} else {
+  console.log("Whole First");
+}
+
+//Functions
+
+function greet(name) {
+  console.log("Hello  " + name);
+}
+
+greet("Yubraj Dhakal Upamanyu");
+
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+
+const addedValue = sum(5, 6);
+console.log(addedValue);
