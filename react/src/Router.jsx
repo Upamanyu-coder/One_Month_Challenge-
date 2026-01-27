6c7f56e843e801effa2ecbea15d18a95bd71ee57
Yubraj Dhakal (Upamanyu-coder) <Upamanyu-coder@users.noreply.github.com>
2026-01-27 17:37:14 +0545

    group routing

--- react/src/Router.jsx
+++ react/src/Router.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import App from "./App";
 import ContactPage from "./pages/Contact";
 import AboutPage from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Products from "./pages/Products";
+import ProductDetailsPage from "./pages/details";
+import ProductCart from "./pages/Cart";
 
 const Router = () => {
   return (
@@ -12,9 +14,29 @@ const Router = () => {
         <Route path="/" element={<App />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<NotFound />} />
 
-        <Route Path="/Products" elements={<ProductsPage />} />
+        <Route
+          path="/products"
+          element={
+            <>
+              <img
+                src="https://img.pikbest.com/templates/20240715/e-commerce-product-banner-design-with-green-colors_10669967.jpg!w700wp"
+                style={{
+                  width: "100%",
+                  height: "300px",
+                  Object: "cover",
+                }}
+              />
+              <Outlet />
+            </>
+          }
+        >
+          <Route index element={<Products />} />
+          {/* <Route path=":id" elements={<ProductDetailsPage />} /> */}
+          <Route path="cart" elements={<ProductCart />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ react/src/pages/details.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProductDetailsPage = () => {
+  return (
+    <div>
+      <>
+        <h1>This is the page of Products Details</h1>
+      </>
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
